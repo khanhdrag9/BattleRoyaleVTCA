a5339a5d7a0883859e0d6744ad2d8761d63463b3
khanhdrag9 <tkhanh783@gmail.com>
2020-05-19 22:07:14 +0700

    Basic movement, rotation

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity;
+    public Transform body;
+
+    private float rotateX = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.localRotation = Quaternion.Euler(rotateX, 0f, 0f);
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        
+        rotateX -= mouseY;
+        rotateX = Mathf.Clamp(rotateX, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(rotateX, 0f, 0f);
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 motion = transform.forward * z + transform.right * x;
+        controller.Move(motion * speed * Time.deltaTime);
+    }
+}
