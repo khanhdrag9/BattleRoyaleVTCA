70a865c16afe1b71be6c445be96bf1a5e9512067
khanhdrag9 <tkhanh783@gmail.com>
2020-05-31 12:48:48 +0700

    Added Colliders

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+
+    CharacterController controller;
+    Health health;
+
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
