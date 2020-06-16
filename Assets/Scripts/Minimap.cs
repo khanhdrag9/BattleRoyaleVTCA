6a31be14baf6fc2d97a4febe9ea0c67c0e1c093c
khanhdrag9 <tkhanh783@gmail.com>
2020-06-16 11:09:17 +0700

    Added Basic minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+    Camera cam;    
+
+    public float HeightView => cam.orthographicSize;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        float screenW = Screen.width;
+        float screenH = Screen.height;
+
+        float newW = cam.rect.height * screenH / screenW;
+        float newX = cam.rect.x + cam.rect.height - newW;
+
+        cam.rect = new Rect(newX, cam.rect.y, newW, cam.rect.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
---
+++ Assets/Scripts/MinimapTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapTarget : MonoBehaviour
+{
+    public Transform target;
+    public float scalePerUnitHeight;
+    public Minimap minimap;
+
+    void Start()
+    {
+        minimap = FindObjectOfType<Minimap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            transform.position = target.position + Vector3.up * 10;
+            transform.localScale = Vector3.one * scalePerUnitHeight * minimap.HeightView;
+        }
+    }
+}
